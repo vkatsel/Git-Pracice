de8c57d33360c08d393dd3d2f56e602c28f0de06
vkatsel <vkatsel@kse.org.ua>
2026-03-10 00:42:17 +0200

    Add main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+def can_get_funding(grades):
+    # TODO: Implement function to check 1 student
+    pass
+
+students_grades = [[100, 90, 71, 60, 61, 95],
+                   [85, 80, 87, 98, 95, 78],
+                   [59, 70, 67, 88, 65, 58]]
+
+
+# TODO: Write a simple algorithm to find out:
+# 1. How many students got funding
+# 2. What was the average GPA
+# 3. What was the average GPA among those, who got funding
