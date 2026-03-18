1b2517926fae88933d0017cafb14b6f250036fc5
Artem Khlusov <artem.khlusov.25@kse.org.ua>
2026-03-18 12:39:42 +0200

    Implemented some serious changes in the algorithm, added a new way to calculate student's GPA and announce whether he passes or no the second time.

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ students_grades = [[100, 90, 71, 60, 61, 95],
                    [85, 80, 87, 98, 95, 78],
                    [59, 70, 67, 88, 65, 58]]
 
+count = 0
+avg_get_fund_gpa = 0
+avg_gpa = 0
+
 def can_get_funding(grades):
     avg_grade = sum(grades)/len(grades)
     if avg_grade <= 80:
@@ -9,8 +13,10 @@ def can_get_funding(grades):
     else:
         return True
 
-passed = can_get_funding(students_grades[0])
-if passed:
-    print("Students gets funding")
-else:
-    print("No")
+for i in range(len(students_grades)):
+    avg_gpa = avg_gpa + sum(students_grades[i]) / len(students_grades[i])
+    if can_get_funding(students_grades[i]):
+        avg_get_fund_gpa = avg_get_fund_gpa + sum(students_grades[i]) / len(students_grades[i])
+        count += 1
+
+print(count, round(avg_gpa/len(students_grades), 2), round(avg_get_fund_gpa/count, 2))
