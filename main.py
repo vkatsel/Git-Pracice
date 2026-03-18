f754ee387475f76c32135f4bcb11ada0c9ce982e
Maksym Larchenko <maksym.larchenko.25@kse.org.ua>
2026-03-18 12:22:57 +0200

    Rewritten the solution from the practice

--- main.py
+++ main.py
@@ -2,20 +2,22 @@ students_grades = [[100, 90, 71, 60, 61, 95],
                    [85, 80, 87, 98, 95, 78],
                    [59, 70, 67, 88, 65, 58]]
 def can_get_funding(grades):
-    # TODO: Implement function to check 1 student
-    sum = 0
-    less = 0
-    for i in range(6):
-        sum += grades[i]
-    GPA = sum/6
-    for p in range(6):
-        if grades[p] < 60:
-            less += 1
-    if GPA >= 80 and less == 0:
-        return True
-    else: return False
+    avr_grade = sum(grades) / len(grades)
+    if avr_grade <= 80:
+        return False
+    else:
+        if min(grades) < 60:
+            return False
+        else:
+            return True
 
-print(can_get_funding(students_grades[2]))
+passed = can_get_funding(students_grades[2])
+if passed:
+    print("Student can get funding")
+else:
+    print("No")
+
+
 
 
 
