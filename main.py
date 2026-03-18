2e7bddba336c1e527a8bd303f8aafa79c4a3288f
Vlad <sevcenkovlad407@gmail.com>
2026-03-18 12:39:15 +0200

    add statistics count

--- main.py
+++ main.py
@@ -9,15 +9,22 @@ def can_get_funding(grades):
         else:
             return True
 
+
 students_grades = [[100, 90, 71, 60, 61, 95],
                    [85, 80, 87, 98, 95, 78],
                    [59, 70, 67, 88, 65, 58]]
 
-passed = can_get_funding(students_grades[0])
-if passed:
-    print("Student gets funding")
-else:
-    print("No")
+cnt_pass = 0
+sum_gpa = 0
+sum_cool_gpa = 0
+for i in range(len(students_grades)):
+    sum_gpa = sum_gpa + sum(students_grades[i])/len(students_grades[i])
+    if can_get_foundation(students_grades[i]):
+        sum_cool_gpa = sum_cool_gpa + sum(students_grades[i])/len(students_grades[i])
+        cnt_pass += 1
+
+print(cnt_pass, round(sum_gpa/len(students_grades), 2), round(sum_cool_gpa/cnt_pass, 2))
+
 # TODO: Write a simple algorithm to find out:
 # 1. How many students got funding
 # 2. What was the average GPA
