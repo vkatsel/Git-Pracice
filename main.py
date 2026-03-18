54203116afe64a5f223468c86326127f1166bc1d
Oksana-Duda <oksana.duda.25@kse.org.ua>
2026-03-18 12:10:22 +0200

    rounded the results

--- main.py
+++ main.py
@@ -17,7 +17,9 @@ for i in range(len(students_grades)):
     if can_get_funding(students_grades[i]):
         sum_cool_gpa = sum_cool_gpa + sum(students_grades[i])/len(students_grades[i])
         cnt_pass += 1
-print(cnt_pass, sum_gpa/len(students_grades), sum_cool_gpa/len(students_grades))
+
+print(cnt_pass, round(sum_gpa/len(students_grades), 2), round(sum_cool_gpa/len(students_grades), 2))
+
 # 1. How many students got funding
 # 2. What was the average GPA
 # 3. What was the average GPA among those, who got funding
