06f6813f8b3403bb071e2b195215ca1935e1da15
Maksym Telnyi <maksym.telnyi.25@kse.org.ua>
2026-03-18 12:30:33 +0200

    hmm... added the last todo task I did....

--- main.py
+++ main.py
@@ -25,7 +25,14 @@ for i in range(len(students_grades)):
 print(gpa_list)
 print(f"Average gpa: {sum(gpa_list)/len(gpa_list)}")
 
+gpa_passed_list=[]
 
+for i in range(len(students_grades)):
+    if can_get_funding(students_grades[i])==True:
+        gpa_passed_list.append(sum(students_grades[i])/len(students_grades[i]))
+
+print(gpa_passed_list)
+print(f"Average GPA among those, who got funding: {sum(gpa_passed_list)/len(gpa_passed_list)}")
 
 # TODO: Write a simple algorithm to find out:
 # 1. How many students got funding
