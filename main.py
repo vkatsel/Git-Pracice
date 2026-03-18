a16c8aaef0b56f99933c24bdee9d783366f2f463
Ihor But <ihor.but.25@kse.org.ua>
2026-03-18 12:08:09 +0200

    Implemented can_get_funding function

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
-def can_get_funding(grades):
-    # TODO: Implement function to check 1 student
-    pass
+from numpy.ma.core import true_divide
 
+
+def can_get_funding(grades):
+    a = sum(grades)/len(grades)>80
+    b = True
+    for grade in grades:
+        if grade<60:
+            b = False
+    return a and b
+
+# def gpa(grades):
+#     return
 students_grades = [[100, 90, 71, 60, 61, 95],
                    [85, 80, 87, 98, 95, 78],
                    [59, 70, 67, 88, 65, 58]]
-
+# c = 0
+for student in students_grades:
+    print(can_get_funding(student))
+    # c+=1
 
 # TODO: Write a simple algorithm to find out:
+
 # 1. How many students got funding
 # 2. What was the average GPA
 # 3. What was the average GPA among those, who got funding
