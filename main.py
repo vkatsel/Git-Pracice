2ff30fc0638332c62a4b2709beb94345be664070
o.semenov <oleksii.semenov.25@kse.org.ua>
2026-03-18 12:37:09 +0200

    def students_passed, sum_gpa, sum_pass_gpa

--- main.py
+++ main.py
@@ -16,6 +16,15 @@ if passed:
     print("students get funding")
 else:
     print("NO")
+students_passed = 0
+sum_gpa = 0
+sum_pass_gpa = 0
+for i in range(len(students_grades)):
+    sum_gpa = sum_gpa + sum(students_grades[i])/len(students_grades[i])
+    if can_get_funding(students_grades[i]):
+        sum_pass_gpa = sum_pass_gpa + sum(students_grades[i])/len(students_grades[i])
+        students_passed += 1
+print(students_passed, sum_gpa, sum_pass_gpa)
 # TODO: Write a simple algorithm to find out:
 # 1. How many students got funding
 # 2. What was the average GPA
