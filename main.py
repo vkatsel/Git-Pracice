861b4c0acfe4b837786601965279802d3f719ec1
Vlad <sevcenkovlad407@gmail.com>
2026-03-18 12:23:11 +0200

    implement can_get_funding()

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
 def can_get_funding(grades):
     # TODO: Implement function to check 1 student
-    pass
+    avg_grade = sum(grades) / len(grades)
+    if avg_grade <= 80:
+        return False
+    else:
+        if min(grades) < 60:
+            return False
+        else:
+            return True
 
 students_grades = [[100, 90, 71, 60, 61, 95],
                    [85, 80, 87, 98, 95, 78],
                    [59, 70, 67, 88, 65, 58]]
 
-
+passed = can_get_funding(students_grades[0])
+if passed:
+    print("Student gets funding")
+else:
+    print("No")
 # TODO: Write a simple algorithm to find out:
 # 1. How many students got funding
 # 2. What was the average GPA
 # 3. What was the average GPA among those, who got funding
+
+#
+# # print(students_grades[0][4])
+# total = 0
+# sum = 0
+# for i in range(len(students_grades)):
+#     for j in range(len(students_grades[i])):
+#         total += students_grades[i][j]
+#         sum += 1
+#
